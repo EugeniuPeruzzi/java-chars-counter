e5ccf352f89789c46a0a5de1dccc8a57b78f7329
EugeniuPeruzzi <jaka1997@live.it>
2023-11-17 14:13:49 +0100

    first commit

---
+++ src/org/lessons/java/charscounter/Main.java
@@ -0,0 +1,18 @@
+package org.lessons.java.charscounter;
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Inserisci na parola che piace a te");
+		String uInput = in.nextLine();
+		
+		
+
+	}
+
+}
